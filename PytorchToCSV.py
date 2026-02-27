55fefef1e1ba4e3084f5de79f4ddeb51613c9dd9
Quadblueshadow <quadblueshadow@gmail.com>
2026-02-26 19:27:24 -0500

    Added pytorch to cpp library capabilities

---
+++ PytorchToCSV.py
@@ -0,0 +1,54 @@
+import torch.nn as nn
+
+class Model(nn.Module):
+    def __init__(self):
+        super(Model, self).__init__()
+
+        self.layers = nn.Sequential(
+            nn.Linear(2, 5),
+            nn.LeakyReLU(),
+            nn.Linear(5, 1),
+        )
+
+    def forward(self, x):
+        return self.layers(x)
+
+model = Model()
+
+list_of_params = list(model.parameters())
+# Access the weights of the first layer (for example)
+print(list_of_params)
+
+activations = 1
+
+with open('net.csv', 'w', newline='') as csvfile:
+    #writer = csv.writer(csvfile)
+
+    l = 0
+    for i in range(0, len(list_of_params), 2):
+        print("Layer " + str(l + 1) + ":")
+        csvfile.write("Layer," + str(l + 1) + "\n")
+
+        for x in range(len(list_of_params[i][0])):
+            print("    Neuron " + str(x + 1) + ":")
+            csvfile.write("Neuron," + str(x + 1) + "\n")
+
+            for z in range(len(list_of_params[i])):
+                print("        Weight " + str(z + 1) + ": " + str(list_of_params[i][z][x]))
+                csvfile.write("Weight," + str(x + 1) + "," + str(list_of_params[i][z][x].item()) + "\n")
+            
+            if i == 0:
+                print("        Bias " + str(x + 1) + ": " + "0")
+                csvfile.write("Bias," + str(x + 1) + ",0" + "\n")
+            else:
+                print("        Bias " + str(x + 1) + ": " + str(list_of_params[i-1][x]))
+                csvfile.write("Bias," + str(x + 1) + "," + str(list_of_params[i-1][x].item()) + "\n")
+
+        csvfile.write("\n")
+
+        if activations > 0:
+            l += 1
+            csvfile.write("Layer," + str(l + 1) + "\n")
+            activations -= 1
+
+        l += 1
--- TestingFile.cpp
+++ TestingFile.cpp
@@ -14,43 +14,45 @@
 
 int main() {
     std::vector<std::shared_ptr<NetComponent>> layers;
-    layers.push_back(std::make_shared<LinearLayer>(1, 1));
-    //layers.push_back(std::make_shared<LeakyRelu>());
-    layers.push_back(std::make_shared<LinearLayer>(1, 1));
+    layers.push_back(std::make_shared<LinearLayer>(1, 2));
+    layers.push_back(std::make_shared<LeakyRelu>());
+    layers.push_back(std::make_shared<LinearLayer>(2, 1));
 
     Model model(layers);
     L2Loss loss_fn;
     Adam optim(&model, 0.01);
 
-    std::vector<float> targets;
+    model.save_net("Net");
 
-    for (int i = 0; i < 10; i++){
-        targets.push_back(i*2 + 1);
-    }
+    // std::vector<float> targets;
 
-    std::vector<float> prediction;
+    // for (int i = 0; i < 10; i++){
+    //     targets.push_back(i*2 + 1);
+    // }
 
-    for (int x = 0; x < 600; x++){
-        for (int i = 0; i < targets.size(); i++){
-            prediction = model.run({(float)i});
+    // std::vector<float> prediction;
 
-            loss_fn.calculate(prediction, {targets[i]});
-            model.backprop(loss_fn.derivative());
+    // for (int x = 0; x < 600; x++){
+    //     for (int i = 0; i < targets.size(); i++){
+    //         prediction = model.run({(float)i});
 
-            optim.apply_changes(0.5);
-        }
-    }
+    //         loss_fn.calculate(prediction, {targets[i]});
+    //         model.backprop(loss_fn.derivative());
 
-    for (int i = 0; i < targets.size(); i++){
-        prediction = model.run({(float)i});
+    //         optim.apply_changes(0.5);
+    //     }
+    // }
 
-        std::cout << "Prediction: " << prediction[0] << ", Target:" << targets[i] << ", Loss:" << loss_fn.calculate(prediction, {targets[i]}) << std::endl;
-        model.backprop(loss_fn.derivative());
+    // for (int i = 0; i < targets.size(); i++){
+    //     prediction = model.run({(float)i});
 
-        optim.apply_changes(0.5);
-    }
+    //     std::cout << "Prediction: " << prediction[0] << ", Target:" << targets[i] << ", Loss:" << loss_fn.calculate(prediction, {targets[i]}) << std::endl;
+    //     model.backprop(loss_fn.derivative());
+
+    //     optim.apply_changes(0.5);
+    // }
     
-    std::this_thread::sleep_for(std::chrono::seconds(10)); 
+    // std::this_thread::sleep_for(std::chrono::seconds(10)); 
     
     return 0;
 }
